66310b37b5bf3f4fc339b4760ae58dfcaf3a9ce8
Vibha27 <svibha081@gmail.com>
2025-01-14 23:22:52 +0530

    implemented binary search using while loop

---
+++ searching/binarySearch.js
@@ -0,0 +1,15 @@
+function binarySearch (array, target) {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        if (target === array[mid]) return mid
+        else if (target < array[mid]) right = mid-1;
+        else left = mid + 1
+    }
+    return -1
+}
+
+const arr = [1,2,3,4,5]
+console.log(binarySearch(arr, 21))
