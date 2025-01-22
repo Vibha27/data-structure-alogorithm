12f917cf00bef6771ec1da81bdd0b2560ab7e94a
Vibha27 <svibha081@gmail.com>
2025-01-22 22:32:11 +0530

    binary search using recursion

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -11,5 +11,17 @@ function binarySearch (array, target) {
     return -1
 }
 
+function binaryRecursiveSearch (array, target, left=0, right=array.length-1) {
+    if (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        if (target === array[mid]) return mid
+        else if (target < array[mid]) return binaryRecursiveSearch(array, target, left, mid-1)
+        else if (target > array[mid]) return binaryRecursiveSearch(array, target, mid+1, right)
+    } else {
+        return -1
+    }
+}
+
 const arr = [1,2,3,4,5]
-console.log(binarySearch(arr, 21))
+console.log(binarySearch(arr, 2))
+console.log(binaryRecursiveSearch(arr, 2))
